a3eb07163132a5e419daf7f0977e77ecf584186a
Aaron Reisman <aaron@reisman.org>
2019-07-01 23:38:02 -0500

    Added a record

---
+++ manifests/record/a.pp
@@ -0,0 +1,21 @@
+define bind::record::a (
+  Stdlib::Fqdn                            $domain,
+  Stdlib::String                          $record_name,
+  Optional[Stdlib::String]                $ttl = undef,
+  Bind::Zone::Class                       $class = 'IN',
+  Stdlib::IP::Address::V4                 $ip_address,
+) {
+
+  $record_template_params = {
+    record_name  => $record_name,
+    ttl          => $ttl,
+    record_class => $class,
+    ip_address   => $ip_address
+  }
+
+  concat::fragment { "bind-zone-record-a-${title}":
+    order   => '30',
+    target  => "bind-zone-${domain}",
+    content => epp('bind/record/a.epp', $record_template_params),
+  }
+}
---
+++ types/record/type.pp
@@ -0,0 +1,1 @@
+type Bind::Record::Type = Enum['a', 'aaaa', 'alias', 'cname', 'mx', 'ns', 'ptr', 'soa', 'srv', 'txt']
