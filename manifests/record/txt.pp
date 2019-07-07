fbd2e688ff652da1c3b0c678928374019b7c9738
Aaron Reisman <aaron@reisman.org>
2019-07-07 16:12:04 -0500

    added txt records

---
+++ manifests/record/txt.pp
@@ -0,0 +1,21 @@
+define bind::record::txt (
+  Stdlib::Fqdn              $domain,
+  String                    $key,
+  String                    $value,
+  Optional[Bind::Zone::Ttl] $ttl = undef,
+  Bind::Zone::Class         $class = 'IN',
+) {
+
+  $record_template_params = {
+    key          => $key,
+    ttl          => $ttl,
+    record_class => $class,
+    value        => $value
+  }
+
+  concat::fragment { "bind-zone-${domain}-a-${title}":
+    order   => '90',
+    target  => "bind-zone-${domain}",
+    content => epp('bind/record/a.epp', $record_template_params),
+  }
+}
